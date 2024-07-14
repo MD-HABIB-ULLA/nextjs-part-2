c92d3669ac2620badc797eda002e5e4316a9d879
habib ulla <habibulla1278@gmail.com>
2024-07-14 20:03:14 +0600

    frist api created

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.themealdb.com'],
-    },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.themealdb.com',
+        port: '',
+        
+      },
+    ],
+  },
 };
 
 export default nextConfig;
