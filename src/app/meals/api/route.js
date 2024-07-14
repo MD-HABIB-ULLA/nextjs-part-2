c61c46819938797fba6027a497f39322b913baa9
habib ulla <habibulla1278@gmail.com>
2024-07-14 20:26:37 +0600

    frist post method added

--- src/app/meals/api/route.js
+++ src/app/meals/api/route.js
@@ -1,5 +1,44 @@
 export async function GET() {
-    return  Response.json({
-        message: "hello world"
+    return Response.json({
+        data
     })
 }
+export async function POST(req) {
+    const newData = await req.json()
+    data.push(
+        {
+            id: data.length + 1,
+            name: newData.name
+        }
+    )
+
+    return Response.json({
+        message: "name added ",
+        data
+    })
+}
+
+
+
+const data = [
+    {
+        id: 1,
+        name: "habib ulla"
+    },
+    {
+        id: 2,
+        name: "rakib shawon "
+    },
+    {
+        id: 3,
+        name: "Towhid ahmed"
+    },
+    {
+        id: 4,
+        name: "rakib lombu "
+    },
+    {
+        id: 5,
+        name: "siam ahmed "
+    },
+]
